ad7bf25f4bc4e24c38abd0ebcb4153731ffc398b
Evan Leis <evan@qubemobile.com>
2011-06-10 11:24:27 -0600

    Logging only initializes once

--- kayako/tests/__init__.py
+++ kayako/tests/__init__.py
@@ -11,27 +11,31 @@ Created on May 5, 2011
 '''
 
 import logging
+import sys
 import unittest
 
-# TODO: Local testing
+LOG_NAME = 'kayako_test'
+log = logging.getLogger(LOG_NAME)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s KAYAKO-TEST  %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
+
+log = logging.getLogger('kayako')
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)-15s KAYAKO-%(levelname)-5s %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
 
 class KayakoTest(unittest.TestCase):
 
-    LOG_NAME = 'kayako_test'
-
     def log(self, *args, **kwargs):
-        log = logging.getLogger(KayakoTest.LOG_NAME)
+        log = logging.getLogger(LOG_NAME)
         log.debug(*args, **kwargs)
+        sys.stderr.flush()
 
-    def setUp(self, *args, **kwargs):
-        log = logging.getLogger(KayakoTest.LOG_NAME)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s KAYAKO-TEST  %(message)s')
-        handler.setFormatter(formatter)
-        log.addHandler(handler)
-        log.setLevel(logging.DEBUG)
-
-        unittest.TestCase.setUp(self, *args, **kwargs)
 
 
 class KayakoAPITest(KayakoTest):
@@ -44,14 +48,3 @@ class KayakoAPITest(KayakoTest):
     def api(self):
         from kayako.api import KayakoAPI
         return KayakoAPI(self.API_URL, self.API_KEY, self.SECRET_KEY)
-
-    def setUp(self, *args, **kwargs):
-
-        log = logging.getLogger('kayako')
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)-15s KAYAKO-%(levelname)-5s %(message)s')
-        handler.setFormatter(formatter)
-        log.addHandler(handler)
-        log.setLevel(logging.DEBUG)
-
-        return KayakoTest.setUp(self, *args, **kwargs)
